4437f684646051d423c78f9efc5522bd0a7994a5
darya zata <daryazata@Mac.fritz.box>
2026-06-21 18:58:39 +0200

    fmw-edit-7_edit-info-cards

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
   def index
     @investors = Investor.all.order(created_at: :desc)
 
-    @total_raised = @investors.sum(:commitment_amount)
+    @potential_capital= @investors.where.not(status: [ "Committed", "Passed" ]).sum(:commitment_amount)
+    @total_raised = @investors.where(status: [ "Committed", "Passed" ]).sum(:commitment_amount)
     @investor_count = @investors.count
-    @committed_count = @investors.where(status: "Committed").count
+    @committed_count = @investors.where(status: [ "Committed", "Passed" ]).count
   end
 end
