7dad9b221d14ec9397900e1ab8a4879f50b213ba
darya zata <daryazata@daryas-MacBook-Air.local>
2026-06-25 00:04:09 +0200

    fmw-feat-18_add-theme-icons

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -1,11 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["moon", "sun"]
 
-  toggle() {
+ connect() {
+  this.updateIcon()
+ }
+
+ toggle() {
     document.documentElement.classList.toggle("dark")
 
-    const isDark = document.documentElement.classList.contains("dark")
-    localStorage.setItem("theme", isDark ? "dark" : "light")
+    const isDark =
+      document.documentElement.classList.contains("dark")
+
+    localStorage.setItem(
+      "theme",
+      isDark ? "dark" : "light"
+    )
+
+    this.updateIcon()
   }
+
+  updateIcon() {
+    const isDark =
+      document.documentElement.classList.contains("dark")
+
+    this.moonTarget.classList.toggle("hidden", isDark)
+    this.sunTarget.classList.toggle("hidden", !isDark)
+}
 }
